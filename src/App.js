f51f5d66c124406b5f8781df9f2d9ced6c5feced
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-04 16:03:11 +0430

    Input text status handler and submit status handler added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
       <TodoList />
     </div>
   );
