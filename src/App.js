6154f93b6ba9d0bbf5f4cce6209e7a0126bd8ced
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-04 15:30:28 +0430

    Form and List components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
