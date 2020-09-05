9e707d1651b13cb283c192f9d73d169ed9833f15
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-05 14:08:48 +0430

    Complete functionality and style added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,10 +4,20 @@ const Todo = ({text,setTodos,todos,todo}) => {
 	const deleteHandler = () => {
 		setTodos(todos.filter(element => element.id !== todo.id));
 	};
+	const completeHandler = () => {
+		setTodos(todos.map(item => {
+			if(item.id === todo.id){
+				return {
+					...item, completed: !item.completed
+				};
+			}
+			return item;
+		}))
+	}
 	return(
 		<div className="todo">
-			<li className="todo-item">{text}</li>
-			<button className="complete-btn">
+			<li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+			<button onClick={completeHandler} className="complete-btn">
 				<i className="fas fa-check"></i>
 			</button>
 			<button onClick={deleteHandler} className="trash-btn">
