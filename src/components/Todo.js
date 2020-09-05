32e89bd8a3d8a8f66a60e677c35b7a2ed52f4207
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-05 13:37:56 +0430

    Remove functionality added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-const Todo = ({text}) => {
+const Todo = ({text,setTodos,todos,todo}) => {
+	const deleteHandler = () => {
+		setTodos(todos.filter(element => element.id !== todo.id));
+	};
 	return(
 		<div className="todo">
 			<li className="todo-item">{text}</li>
 			<button className="complete-btn">
 				<i className="fas fa-check"></i>
 			</button>
-			<button className="trash-btn">
+			<button onClick={deleteHandler} className="trash-btn">
 				<i className="fas fa-trash"></i>
 			</button>
 		</div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Todo from "./Todo"
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos,setTodos}) => {
 	return(
 	<div className="todo-container">
       <ul className="todo-list">
       	{todos.map(todo => (
-      		<Todo text={todo.text} key={todo.id} />
+      		<Todo todo={todo} text={todo.text} key={todo.id} setTodos={setTodos} todos={todos} />
       	))}
       </ul>
     </div>
